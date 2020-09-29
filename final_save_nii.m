ee7d1284b296350b3fd80bf05374a9420228d9a9
yanlong-sun <yanlongs@usc.edu>
2020-09-29 16:16:15 -0700

    save_nii

--- final_save_nii.m
+++ final_save_nii.m
@@ -4,13 +4,26 @@ clear;
 mat = load('../predictions/1663535/predictions.mat');
 
 %% Pred Part
-name = mat.name;
-image = mat.image;
 pred = mat.pred;
+pred_nii = make_nii(pred);
+pred_img2 = pred_nii.img;
+[n1,n2,n3] = size(pred_img2);
+pred_img = zeros(size(pred_img2));
 
-
-
-
+slice_number_long = 10000;
+for j = 1 : n1
+    pred_img(:, :, j) = pred_img2(j, :, :);
+    save_path = ['../predictions/1663535/test/', num2str(slice_number_long + i), '.png'];
+    imwrite(pred_img( :,:, j), save_path);
+    if j == 1
+        preds = imread(save_path);
+        preds = imcrop(preds, [1, 64, 255, 127]);
+    end
+    single_pred = imread(save_path);
+    single_pred = imcrop(single_pred, [1, 64, 255, 127]);
+    imshow(single_pred)
+    preds = cat(3, preds, single_pred);
+end
 
 %% Original Image Part
 test_data_tif_path = './data/test/';     
@@ -27,6 +40,7 @@ for i = 4: length(slicefileNames)
     for j = 3 : 2 : length(slices_list)
         single_slice = imread([slices_dir_path, slices_list(j).name(1:13),'.tif']);
         single_slice = imcrop(single_slice, [1, 64, 255, 127]);
+        imshow(single_slice)
         slices = cat(3, slices, single_slice);
     end
 end
@@ -38,7 +52,7 @@ v_orig = load_nii('../test_data_nii/1663535.nii.gz');
 v2 = v_orig;
 v3 = v_orig;
 v2.img = slices;
-v3.img = pred_final;
+v3.img = preds;
 save_nii(v2, '../predictions/1663535/image.nii');
 save_nii(v3, '../predictions/1663535/pred.nii');
 
