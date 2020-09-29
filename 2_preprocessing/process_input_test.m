9bc94f7b1c9912d864116d158972430a92c8c64d
yanlong-sun <yanlongs@usc.edu>
2020-09-29 00:40:23 -0700

    sorted

--- 2_preprocessing/process_input_test.m
+++ 2_preprocessing/process_input_test.m
@@ -6,15 +6,25 @@ test_data_tif_path = '../../test_data_tif/';
 slicedirOutput=dir(test_data_tif_path);
 slicefileNames={slicedirOutput.name};
 
+mat = load('../../predictions/1663535/predictions.mat');
 
-for i = 3: length(slicefileNames)
+name = mat.name;
+name_size = size(name);
+name_num = zeros(1, name_size(1));
+for j = 1 : name_size(1)
+    name_num(j) = str2double(name(j, 9:11));
+end
+[name_sorted, index] = sort(name_num);
+
+for i = 4: length(slicefileNames)
     slices_case_name = char(string(slicefileNames(i)));  
     slices_dir_path = [test_data_tif_path, slices_case_name, '/'];
     slices_list = dir(strcat(slices_dir_path,'*.tif')); 
 
-    slices = imread([slices_dir_path, slices_list(1).name]);
-    for j = 2 : length(slices_list)
-        single_slice = imread([slices_dir_path, slices_list(j).name]);
+    slices = imread([slices_dir_path, slices_list(index(1)).name]);
+    
+    for j = 2 : length(index)
+        single_slice = imread([slices_dir_path, slices_list(index(j)).name]);
         slices = cat(3, slices, single_slice);
     end
 end
--- try22.m
+++ try22.m
@@ -6,7 +6,7 @@ v_orig = load_nii('../test_data_nii/1663535.nii.gz');
 v = v_orig.img;
 [n1,n2,n3] = size(v); 
 for i = 1 : n3
-    save_path = ['../test_data/', '1663535_', num2str(i), '.tif'];
+    save_path = ['../test_data_tif/test1/', '1663535_', num2str(i), '.tif'];
     imshow(v(:,:,i),[])
     imwrite(v(:,:,i), save_path);
 end
