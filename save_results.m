9e4dfd82a5cd36540fa80d157b66ba40507ae7bc
yanlong-sun <yanlongs@usc.edu>
2020-09-29 21:21:05 -0700

    improve save

--- save_results.m
+++ save_results.m
@@ -1,55 +1,60 @@
 clc;
 clear;
 
-mat = load('../predictions/1663535/predictions.mat');
+prediction_path = '../predictions/';
+test_data_tif_path = '../test_data_tif/';
+pred_folder= dir(prediction_path);
+pred_file={pred_folder.name};
 
-
-%% Original Image Part
-test_data_tif_path = './data/test/';     
-slicedirOutput=dir(test_data_tif_path);
-slicefileNames={slicedirOutput.name};
-for i = 4: length(slicefileNames)
-    slices_case_name = char(string(slicefileNames(i)));  
-    slices_dir_path = [test_data_tif_path, slices_case_name, '/'];
-    slices_list = dir(strcat(slices_dir_path,'*.tif'));
-
-    slices = imread([slices_dir_path, slices_list(1).name(1:13),'.tif']);
-    slices = imcrop(slices, [1,64,255,127]);
+for num_pred= 4 : length(pred_file)
+    case_name = pred_file(num_pred);
+    case_name = char(case_name);
+    mat = load([prediction_path, case_name, '/', case_name, '_predictions.mat']);
     
-    for j = 3 : 2 : length(slices_list)
-        single_slice = imread([slices_dir_path, slices_list(j).name(1:13),'.tif']);
-        single_slice = imcrop(single_slice, [1, 64, 255, 127]);
+    %% Image Part
+    test_data_path = ['./data/test/',case_name, '/']; 
+    slice_dirOutput=dir(test_data_path);
+    slice_fileNames={slice_dirOutput.name};
+    target_size = size(imread([test_data_tif_path, case_name, '/', case_name,'_',  num2str(10001), '.tif' ]));
+    crop_window = centerCropWindow2d([256, 256], target_size);
+    first_slice_path = ['./data/test/',case_name, '/', case_name,'_', num2str(10001), '.tif' ];
+    first_slice = imread(first_slice_path);
+    slices = imcrop(first_slice, crop_window);
+    for i = 2 : (length(slice_fileNames)-2)/2
+        single_slice_path = ['./data/test/',case_name, '/',case_name,'_', num2str(10000+i), '.tif' ];
+        single_slice = imread(single_slice_path);
+        single_slice = imcrop(single_slice, crop_window);
+        figure(4)
         imshow(single_slice)
         slices = cat(3, slices, single_slice);
     end
-end
-
-%% Pred Part
-pred = mat.pred;
-pred_nii = make_nii(pred);   % this nii file has the wrong head
-pred_nii_img = pred_nii.img;
-[n1,n2,n3] = size(pred_nii_img);
-slice_number_long = 10000;
-for j = 1 : n1
-    pred_img(:, :, j) = pred_nii_img(j, :, :);
-    if j == 1
-        preds = mat2gray(pred_img(:, :, j));
-        imshow(preds)
-        preds = imcrop(preds, [1, 64, 255, 127]);
+    %% Pred Part
+    pred = mat.pred;
+    pred_nii = make_nii(pred);   % this nii file has the wrong head
+    pred_nii_img = pred_nii.img;
+    [n1,n2,n3] = size(pred_nii_img);
+    slice_number_long = 10000;
+    for j = 1 : n1
+        pred_img(:, :, j) = pred_nii_img(j, :, :);
+        if j == 1
+            preds = mat2gray(pred_img(:, :, j));
+            imshow(preds)
+            preds = imcrop(preds, crop_window);
+        end
+        single_pred = mat2gray(pred_img(:, :, j));
+        single_pred = imcrop(single_pred, crop_window);
+        imshow(single_pred)
+        preds = cat(3, preds, single_pred);
     end
-    single_pred = mat2gray(pred_img(:, :, j));
-    single_pred = imcrop(single_pred, [1, 64, 255, 127]);
-    imshow(single_pred)
-    preds = cat(3, preds, single_pred);
+
+
+    %% SAVE part
+    v_orig = load_nii(['../test_data_nii/', case_name, '.nii.gz']);
+    v2 = v_orig;
+    v3 = v_orig;
+    v2.img = slices;
+    v3.img = preds;
+    save_nii(v2, [prediction_path, case_name, '/', case_name, '_image.nii']);
+    save_nii(v3, [prediction_path, case_name, '/', case_name, '_pred.nii']);
+
 end
-
-
-%% SAVE part
-v_orig = load_nii('../test_data_nii/1663535.nii.gz');
-v2 = v_orig;
-v3 = v_orig;
-v2.img = slices;
-v3.img = preds;
-save_nii(v2, '../predictions/1663535/image.nii');
-save_nii(v3, '../predictions/1663535/pred.nii');
-
