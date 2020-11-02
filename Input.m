5e213ede55c8910954161e3c7235b023ffd6dd6e
yanlong-sun <yanlongs@usc.edu>
2020-11-02 12:37:42 -0800

    train

--- Input.m
+++ Input.m
@@ -3,9 +3,11 @@ clear;
 
 % Process training data -> add mask path
 % Process test data     -> uncomment 'masks_tif = zeros(size(slices_tif));'
+% train_test_data_slices = '../test_data/slices/';
+% train_test_data_masks = '../test_data/masks/';
 
-train_test_data_slices = '../test_data/slices/';
-train_test_data_masks = '../test_data/masks/';
+train_test_data_slices = '../valid_data/slices/';
+train_test_data_masks = '../valid_data/masks/';
 
 slices_nii_folder=dir(train_test_data_slices);
 slices_nii_file={slices_nii_folder.name};
@@ -22,11 +24,11 @@ for num_nii = 4 : length(slices_nii_file)
      disp(case_name)
     
     v_slices = load_untouch_nii([train_test_data_slices, case_name, '.nii.gz']);  
-    %v_masks = load_untouch_nii([train_test_data_masks, case_name, '.manual.mask.nii.gz']);
+    v_masks = load_untouch_nii([train_test_data_masks, case_name, '_ss.nii.gz']);
     slices_tif = v_slices.img;
-    %masks_tif = v_masks.img;
+    masks_tif = v_masks.img;
     
-    masks_tif = zeros(size(slices_tif));
+    %masks_tif = zeros(size(slices_tif));
     
     [n1,n2,n3] = size(slices_tif);
     
@@ -46,9 +48,9 @@ for num_nii = 4 : length(slices_nii_file)
     
 %% 
     %destination_path = './data/test_model/';    % Get qualitative results
-    %destination_path = './data/valid/'; 
+    destination_path = './data/valid/'; 
     %destination_path = './data/train/'; 
-    destination_path = ['./data/test/', case_name, '/'];
+    %destination_path = ['./data/test/', case_name, '/'];
      
 %% classify into two categories    
     if max(max(max(slices_tif))) > 1220
