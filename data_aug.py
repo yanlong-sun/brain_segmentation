fe900e9a27f07f9b40263b942f16ccbecb4a14b2
yanlong-sun <yanlongs@usc.edu>
2021-01-14 00:14:11 -0800

    aug

--- data_aug.py
+++ data_aug.py
@@ -12,14 +12,15 @@ def data_aug():
     unaug_training_image_path = './data/train/'
     aug_training_image_path = './data/aug_train/'
     imgs_train, imgs_mask_train, names = load_data(unaug_training_image_path)
-    print(names)
-    """
     aug_imgs_train = seq.augment_images(imgs_train)
     aug_imgs_mask_train = seq.augment_images(imgs_mask_train)
-    c = 1
+    b, c = 1, 1
+    for each in aug_imgs_train:
+        cv2.imwrite(aug_training_image_path, '%s_mask.tif', b, each)
+        b += 1
     for each in aug_imgs_mask_train:
-        cv2.imwrite(aug_training_image_path, '')
-        """
+        cv2.imwrite(aug_training_image_path, '%s.tif', c, each)
+        c += 1
 
 if __name__ == '__main__':
     data_aug()
